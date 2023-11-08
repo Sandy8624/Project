5d1a06c6842a47cf9c6ff6e985b09d5133bfe63c
Sandy8624 <msanthosh862003@gmail.com>
2023-11-08 22:15:49 +0530

    Add edit and date

--- Todo App/Tpdo.js
+++ Todo App/Tpdo.js
@@ -2,21 +2,35 @@ document.addEventListener("DOMContentLoaded", function () {
     const taskInput = document.getElementById("taskInput");
     const addTaskButton = document.getElementById("addTask");
     const taskList = document.getElementById("taskList");
+    const date = document.getElementById("date");
 
     addTaskButton.addEventListener("click", function () {
         const taskText = taskInput.value.trim();
-        if (taskText !== "") {
+        const taskText1 = date.value.trim();
+        if (taskText, taskText1 !== "") {
             const li = document.createElement("li");
-            li.innerHTML = `
-                ${taskText}
-                <span class="delete">X</span> `;
+
+           
+         li.innerHTML = `
+              <span>${taskText}</span>
+              <span class="date-value">${taskText1}</span>
+              <span class="delete">Delete</span>
+              <span class="edit">Edit</span>`;
+
+            taskList.appendChild(li);
+            taskInput.value = "";
+            date.value = "";
 
             li.querySelector(".delete").addEventListener("click", function () {
                 taskList.removeChild(li);
             });
+            li.querySelector(".edit").addEventListener("click", function () {
+                const editText = (taskInput.value.trim());
+                li.firstChild.textContent = editText.trim();
+                taskInput.value = "";
+            });
 
-            taskList.appendChild(li);
-            taskInput.value = "";
+
         }
     });
 });
